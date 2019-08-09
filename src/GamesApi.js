0daadca6ef9ca1b330ae65d4a869cf7fdd4d24af
MrKamake <seongjooon@gmail.com>
2019-08-09 10:07:01 +0900

    create and modify Api

---
+++ src/GamesApi.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default function getGamesApi() {
+  const apiKey = 'c7t4r5lnktjelu8azlv5meo8saunbm';
+  const urls = 'https://api.twitch.tv/helix/games/top?first=10';
+  return axios({
+    method: 'get',
+    url: urls,
+    headers: { "Client-ID": apiKey }
+  }).then(res => res.data.data);
+}
---
+++ src/VideosApi.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export default function getVideosApi(gameId, arrow, videosData) {
+  if (arrow) {
+    const apiKey = 'c7t4r5lnktjelu8azlv5meo8saunbm';
+    // const urls = `https://api.twitch.tv/helix/videos?game_id=${gameId}&language=en&${arrow}=${videosData.pagination.cursor}&first=20&sort=views`;
+    const urls = `https://api.twitch.tv/helix/videos?game_id=${gameId}&language=ko&${arrow}=${videosData.pagination.cursor}&first=20&sort=trending`;
+    return axios({
+      method: 'get',
+      url: urls,
+      headers: { 'Client-ID': apiKey }
+    }).then(res => res.data);
+  }
+  const apiKey = 'c7t4r5lnktjelu8azlv5meo8saunbm';
+  // const urls = `https://api.twitch.tv/helix/videos?game_id=${gameId}&language=en&first=20&sort=views`;
+  const urls = `https://api.twitch.tv/helix/videos?game_id=${gameId}&language=ko&first=20&sort=trending`;
+  return axios({
+    method: 'get',
+    url: urls,
+    headers: { 'Client-ID': apiKey }
+  }).then(res => res.data);
+
+}
